e318b1cfe123020fad3b6965afd1bceeb8c6634a
CeciliaBdev <cecilia.bernard@live.fr>
2023-02-24 11:31:37 +0100

    update section impots

--- src/components/Impots.js
+++ src/components/Impots.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import services from '../assets/services.png'
 
 function Impots() {
   return (
     <div className="impots" id="impots">
-      <p>test</p>
+      <div className="impots_texte">
+        <h2>En faisant appel à nos services</h2>
+        <div className="credit">
+          <p>Bénéficiez de 50 % * de crédit d'impôt</p>
+        </div>
+        <p>selon l'article 100 sexdecies du code général des impôts</p>
+      </div>
+      <div className="services_personnes">
+        <img src={services} alt="" />
+      </div>
     </div>
   )
 }
